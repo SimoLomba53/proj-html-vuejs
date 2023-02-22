7ab10b46dcac718e75af6080af3e44b092812a4c
SimoLomba53 <simolomba253@gmail.com>
2023-02-22 14:05:48 +0100

    correzione card header e aggiunta di hover

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ createApp({
                 },
             ],
 
-            pagessection: [
+            footerpagessection: [
                 {
                     item: "Blog",
                 },
