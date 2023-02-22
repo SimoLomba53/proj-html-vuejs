3511396c66223c71c814559fd9f475999eb9f412
SimoLomba53 <simolomba253@gmail.com>
2023-02-22 13:34:40 +0100

    footer dinamico

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@ createApp({
             headerelement: [
                 {
                     plan: "Courses",
+
                 },
 
                 {
                     plan: "Zoom",
+
                 },
 
                 {
@@ -31,7 +33,42 @@ createApp({
                 {
                     plan: "Demos",
                 },
-            ]
+            ],
+
+            footerelement: [
+                {
+                    location: "USA,California 20,First Avenue,California",
+                    telephone: "+ 1 212 458 300 32 ",
+                    fax: "+ 1 212 375 24 14 ",
+                    mail: "info@masterstudy.com",
+                    firsttext: "Our main target is to developing Yourself as a Leader",
+                    firstdate: "--August 9,2018",
+                    secondtext: "Those Other College Expenses You Aren't Thinking About",
+                    seconddate: "--June 3,2015",
+                },
+            ],
+
+            pagessection: [
+                {
+                    item: "Blog",
+                },
+                {
+                    item: "Home",
+                },
+                {
+                    item: "Shortcodes",
+                },
+                {
+                    item: "Courses",
+                },
+                {
+                    item: "Membership A.",
+                },
+                {
+                    item: "Typography",
+                },
+            ],
+
         }
     }
 }).mount('#app')
